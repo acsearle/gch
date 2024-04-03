ba6900e6652707005450375f29541d7f9ec43b67
Antony Searle <antony.searle@gmail.com>
2024-04-03 18:55:37 +1100

    Initial Commit

---
+++ gch/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  gch
+//
+//  Created by Antony Searle on 3/4/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
